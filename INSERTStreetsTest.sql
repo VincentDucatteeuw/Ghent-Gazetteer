abd5167cdcc64354e720f87fa3477e6889e266d8
Vincent Ducatteeuw <Vincent.Ducatteeuw@UGent.be>
2021-04-02 14:03:16 +0200

    Create INSERTStreetsTest.sql
    Script for adding 10 test streets to table place.

---
+++ INSERTStreetsTest.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.place (title)
+VALUES
+    ('Kouter'),
+    ('Savaanstraat'),
+	('Grote Huidevettershoek'),
+	('Brabantdam'),
+	('Grote Huidevettershoek'),
+	('Ketelvest'),
+	('Kortedagsteeg'),
+	('Platteberg'),
+	('Sint-Jansvest');
