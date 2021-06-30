816f5b6fe127c427cb6e286f33ac00682fb469a7
Vincent Ducatteeuw <Vincent.Ducatteeuw@UGent.be>
2021-06-30 16:15:53 +0200

    Updated files to ',' delimiter and correct WKT format in straten 2021

--- sql_queries/JOINPlaceLocation.sql
+++
@@ -1,3 +0,0 @@
-SELECT place.placeid, place.title, location.geometry
-FROM public.place
-INNER JOIN public.location ON place.placeid=location.placeid;
--- sql_queries/JOINPlaceToponym.sql
+++
@@ -1,3 +0,0 @@
-SELECT place.placeid, place.title, toponym.toponym, toponym.toponymlanguage
-FROM place
-INNER JOIN public.toponym ON place.placeid=toponym.placeid;
